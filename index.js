bb716ead8d546fd63c500fbd597801524196c82d
ryankissoon <ryankissoon_31@msn.com>
2022-01-15 03:17:16 -0500

    commit tree 62699d0d8245b77f29252bf74f382cea8968f953

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 17;
+let num4 = 13;
+let multiply = (num1 * num2)
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = Math.ceil(num3 / num4) + 2;
+let max = Math.max(1, 2, 20, 4, 8, 2, 0, 9);
